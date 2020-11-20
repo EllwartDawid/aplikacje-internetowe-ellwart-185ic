6cbe6db5140b95368a96aee56fc1e070194f5f6f
EllwartDawid <d.ellwart2@wp.pl>
2020-11-20 22:04:47 +0100

    commit

---
+++ Laboratorium2/konto/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path
+from . import views
+from .views import SignUpView, PasswordsChangeView
+from django.contrib.auth import views as auth_views
+
+# importowanie widoku rejestracji
+urlpatterns = [
+    # Rejestracja
+    path('signup/', SignUpView.as_view(), name='signup'),
+
+    # Zmiana hasła + kiedy pomyślnie zmieinone
+    path('change-password/', PasswordsChangeView.as_view(template_name = "registration/change_password.html"), name='change_password'),
+    path('change_password_done/', views.changed, name='change_password_done'),
+
+    # Resetowanie hasła
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset_form'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+]
