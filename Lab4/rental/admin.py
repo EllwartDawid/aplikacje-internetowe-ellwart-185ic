2f5842405dd2e908d8946e15a26a8c75dc071a75
EllwartDawid <d.ellwart2@wp.pl>
2020-12-14 19:51:25 +0100

    ss

--- Lab4/rental/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Friend, Belonging, Borrowed
-
-#Dodajemy tutaj nasze Posty do panelu administracynego, by móc je tam zobaczyć
-admin.site.register(Friend)
-admin.site.register(Belonging)
-admin.site.register(Borrowed)
--- Lab5/mysite/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from . import views
-from django.views.generic.base import TemplateView
-
-urlpatterns = [
-    path('admin/', admin.site.urls),    
-    path('', include('konto.urls')),
-]
